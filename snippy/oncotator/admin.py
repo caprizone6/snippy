405f7bd5240615e678cf3482e95b13b9dbe4facb
caprizone6 <caprizone6@gmail.com>
2018-10-06 00:07:06 -0400

    2018 - update

--- snippy/oncotator/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CSVFile
-
-class FileAdmin(admin.ModelAdmin):
-	list_display = ['title', 'file', 'result', 'upload_time']
-
-admin.site.register(CSVFile, FileAdmin)
--- snippy/oncotator/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from models import CSVFile
-
-class FileForm(forms.ModelForm):
-
-    class Meta:
-        model = CSVFile
-        fields = ('title', 'file')
